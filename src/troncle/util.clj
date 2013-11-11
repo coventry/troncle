6f33c5e5bf9bf2ab67dd6443115f2a7f9a08be13
Alex Coventry <coventry@gmail.com>
2013-11-10 20:57:58 -0500

    Modify postwalk so it preserves all metadata

--- src/troncle/util.clj
+++ src/troncle/util.clj
@@ -20,3 +20,10 @@
   (w/walk metadata-walk
           #(list form (meta form) %)
           form))
+
+;; Correction for the fact that clojure.walk drops metadata
+(defn walk [inner outer form]
+  (let [i (w/walkt form inner) fm (meta form)]
+    (outer (if fm (with-meta i fm) i))))
+(defn postwalk [f form] (walk (partial postwalk f) f form))
+
