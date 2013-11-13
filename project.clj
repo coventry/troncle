f20ebe97915144136f9f3d006987d663f3dd9509
Alex Coventry <coventry@gmail.com>
2013-11-13 01:34:51 -0500

    Use a personal jar for tools.reader v 0.8.0
    I can't figure out where I got this jar from, but lein's not finding it
    in the usual places.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [nrepl-discover "0.1.0"]
-                 [org.clojure/tools.reader "0.7.10"]
+                 [org.clojars.coventry/tools.reader "0.8.0-SNAPSHOT"]
                  [caribou/clojure.walk2 "0.1.0"]]
   :repl-options {:nrepl-middleware [nrepl.discover/wrap-discover]})
 
