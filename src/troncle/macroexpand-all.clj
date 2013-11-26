accab2d64998d914711bf4fede6c16ac20ab6047
Alex Coventry <coventry@gmail.com>
2013-11-26 16:27:35 -0500

    Clean, tail-pos-preserving wrapping

--- src/troncle/macroexpand-all.clj
+++ src/troncle/macroexpand-all.clj
@@ -6,12 +6,15 @@
             :reload
             ))
 
+(defn recur? [f] (or (and (coll? f) (= (first f) 'recur))
+                     (-> f meta ::recur?)))
+
 (defmacro macroexpand-macro
   [f result-tree]
   (let [ftree   (atom [])
         vftree  (m/assign-var "ftree" ftree)
-        ;; This macroexpansion happens in the compile-time context of
-        ;; the target code, so we get any effects there for free. E.g.
+        ;; compile-time context here is the target code, so we get any
+        ;; effects there for free. E.g.
         ;; (defmacro n [t] (println (macroexpand t) (-> 1 inc)))
         ;; (let [-> (fn [& args] args)] (n (-> 1 inc))) => (-> 1 inc) 2
         me      (macroexpand f)
@@ -26,40 +29,35 @@
   macroexpand-macro phase."
   (fn [f] (if (not (coll? f))
             f
-            ;; (assert (= f' (macroexpand f))) would be natural here,
-            ;; but the whole point of this adventure is that we can't
-            ;; blithely do a macroexpand without the local bindings
-            ;; context.
             (let [[[f' t] & r] @expansions
-                  recur?       (and (list? f')
-                                    (= (first f') 'recur))]
-              (swap! expansions rest)
-              ;; We want to mark forms which contain a recur, because
-              ;; the tracing instrumentation can screw up the tail
-              ;; position.
-              (vary-meta (reconstruct-expansion [f' t] wrapper)
-                         update-in [::recur?] #(or % recur?))))))
+                  ;; Wrong ctx for (macroexpand f), so can't check = f'
+                  _            (swap! expansions rest)
+                  ;; Mark forms which contain a recur, because the tracing
+                  ;; instrumentation can screw up tail position.
+                  f'           (vary-meta f' assoc ::recur? (recur? f'))
+                  rv           (reconstruct-expansion [f' t] wrapper)]
+              (if wrapper (wrapper rv) rv)))))
 
 (defn reconstruct-expansion [[form expansions :as ptree] & [wrapper]]
   (if (empty? @expansions)
     form
     ;; This runs w/walk-wrap on the same form as macroexpand-macro
     ;; did, pulling out the results from there and stitching them into
-    ;; the form.
+    ;; the form, relying on the fact that the call-order is the same.
     (let [replacement (replacement-fn expansions wrapper)
-          rv          (w/walk-wrap replacement (macroexpand form))
-          recur?      (some #(-> % meta ::recur?) rv)
+          rv          (w/walk-wrap replacement form)
+          recur?      (or (some #(-> % meta ::recur?) rv))
           recur-site? (#{'loop* 'fn*} (first rv))
-          ;; We can only screw up the tail position of a form containing
-          ;; recur if we're still inside the recursion point, so keep
-          ;; track of that
-          rv          (vary-meta rv update-in
-                                 [::recur?] (constantly (and recur? (not recur-site?))))]
-      (if wrapper (wrapper rv) rv))))
+          ;; Can only screw up the tail position of a form containing
+          ;; recur if form is still inside the recursion point, so
+          ;; keep track of that
+          recur-mark? (and recur? (not recur-site?))]
+      (vary-meta rv update-in [::recur?] (constantly recur-mark?)))))
 
 (defn get-expansion-info [f]
   (let [a (atom []), av (m/assign-var "tree" a)]
     (eval `(macroexpand-macro ~f ~av))
+    (binding [*print-meta* true] (u/pprint @a))
     (first @a)))
 
 (defn macroexpand-all [f]
@@ -67,14 +65,3 @@
 
 (defn wrap [f wrapper]
   (-> (get-expansion-info f) (reconstruct-expansion wrapper) doall))
-
-(user/starbreak)
-
-(-> '(let [-> (fn [& args] args)] (-> 1 inc))
-    macroexpand-all doall u/pprint)
-
-(def eg '(inc (loop [i 10]
-            (if (= i 0) 11 (recur (dec i))))))
-
-(-> eg (wrap  #(if (not (-> % meta ::recur?)) (list 'do % '(println "here")) %)))
-
