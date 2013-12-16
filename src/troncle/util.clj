afa0870d2349d4b6e43fd4afcbf6b9138f6bde09
Alex Coventry <coventry@gmail.com>
2013-12-16 00:15:25 -0500

    Remove unused test data

--- src/troncle/util.clj
+++ src/troncle/util.clj
@@ -7,15 +7,6 @@
 (defn trace [& args] (apply t/trace args))
 (def deftrace #'t/deftrace)
 
-(def test-strings "(defn ^String capitalize
-  [^CharSequence s]
-  (let [s (.toString s)]
-    (if (< (count s) 2)
-      (.toUpperCase s)
-      (str (.toUpperCase (subs s 0 1))
-           (.toLowerCase (subs s 1))))))
-")
-
 (defn metadata-walk [form]
   (w/walk metadata-walk
           #(list form (meta form) %)
